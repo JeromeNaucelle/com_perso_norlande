187cfc92aeed9f54c82d93410bb8301e5cf3b58b
Jerome Naucelle <Jerome.Naucelle@hsc-labs.com>
2017-01-25 14:29:23 +0100

    ajout de la totalité des colonnes des compétences

--- com_perso_norlande/site/includes/SyntheseCompetences.php
+++ com_perso_norlande/site/includes/SyntheseCompetences.php
@@ -8,6 +8,76 @@ class SyntheseCompetences {
 	private $nom;
 	private $competences;
 	
+	/* Creation d'un tableau de la forme
+		"colonne BDD" => "Label" */
+	private static array(
+	"parcelles" => "Possessions de départs (parcelles)",
+	"possessions_depart" => "Possessions de départs",
+	"a_prevoir" => "Objet de jeu que vous devez prévoir",
+	"connaissances" => "Connaissances",
+	"aide_jeu" => "Aides de jeu",
+	
+	"lecture_ecriture" => "Lecture et écriture",
+	"rumeurs" => "Rumeurs",
+	"actions_guerre" => "Actions de Guerre",
+	"coup_force" => "Coup de force",
+	"voix_noires" => "Voix noires",
+	"voix_blanches" => "Voix blanches",
+	"voix_peuple" => "Voix du peuple",
+	"voix_roi" => "Voix du roi",
+	"veto" => "Veto",
+	"manigance" => "Manigances",
+	"lieux_pouvoir" => "Lieux de pouvoir",
+	"force_physique" => "Force physique",
+	"bonus_mana" => "Bonus de mana",
+	"globes_sortilege" => "Globes de sortilège",
+	"bonus_coups" => "Bonus coups",
+	"bonus_coups_etoffe" => "Bonus coups (étoffe)",
+	"esquive_etoffe" => "Esquive (étoffe)",
+	"resiste_etoffe" => "Résiste (étoffe)",
+	"esquive_cuir" => "Esquive (cuir)",
+	"resiste_cuir" => "Résiste (cuir)",
+	"esquive_maille" => "Esquive (maille)",
+	"resiste_maille" => "Résiste (maille)",
+	"esquive_plaque" => "Esquive (plaque)",
+	"resiste_plaque" => "Résiste (plaque)",
+	"maniement" => "Maniement",
+	"attaque_spe" => "Attaque spéciale",
+	"attaque_spe_tranchant" => "Attaque tranchante",
+	"attaque_spe_contondant" => "Attaque contondante",
+	"attaque_spe_hast" => "Attaque d'hast",
+	"attaque_spe_tir" => "Attaque de tir",
+	"attaque_spe_lancer" => "Attaque de lancer",
+	"sortilege" => "Sortilège",
+	"sort_masse1" => "Sort de masse",
+	"sort_masse2" => "Sort de masse",
+	"immunite_etoffe" => "Immunité (étoffe)",
+	"immunite_cuir" => "Immunité (cuir)",
+	"immunite_plaque" => "Immunité (plaque)",
+	"immunite_maille" => "Immunité (maille)",
+	"amelioration" => "Amélioration",
+	"capacite" => "Capacité", // fusion de capacité et fréquence
+	"technique1" => "Technique", // fusion technique 1(nom), technique 1(cout), technique 1(effet)
+	"technique2" => "Technique", // fusion technique 2(nom), technique 2(cout), technique 2(effet)
+	"piege1" => "Piège", // fusion Piège 1(nom, type, niveau), Piège 1 (coût), Piège 1 (effet)
+	"piege2" => "Piège", // fusion Piège 2(nom, type, niveau), Piège 2 (coût), Piège 2 (effet)
+	"breuvage1" => "Breuvage", // fusion breuvage 1(nom, type, niveau), breuvage 1 (coût), breuvage 1 (effet)
+	"breuvage2" => "Breuvage", // fusion breuvage 2(nom, type, niveau), breuvage 1 (coût), breuvage 1 (effet)
+	"breuvage3" => "Breuvage", // fusion breuvage 3(nom, type, niveau), breuvage 1 (coût), breuvage 1 (effet)
+	"breuvage4" => "Breuvage", // fusion breuvage 4(nom, type, niveau), breuvage 1 (coût), breuvage 1 (effet)
+	"breuvage5" => "Breuvage", // fusion breuvage 5(nom, type, niveau), breuvage 1 (coût), breuvage 1 (effet)
+	"invocation1" => "Invocation", // fusion invocation 1(type, niveau), invocation 1 (coût), invocation 1 (service)
+	"invocation2" => "Invocation", // fusion invocation 2(type, niveau), invocation 2 (coût), invocation 2 (service)
+	"metamorphose" => "Métamorphose",
+	"pouvoir1" => "Pouvoir magique", // fusion Pouvoir magique 1(nom, type, mana), Formule et effet
+	"pouvoir2" => "Pouvoir magique", // fusion Pouvoir magique 1(nom, type, mana), Formule et effet
+	"pouvoir3" => "Pouvoir magique", // fusion Pouvoir magique 1(nom, type, mana), Formule et effet
+	"pouvoir4" => "Pouvoir magique", // fusion Pouvoir magique 1(nom, type, mana), Formule et effet
+	);
+
+	
+	
+	
 	private $lecture_ecriture;
 	private $rumeurs;
 	private $actions_guerre;
@@ -32,6 +102,8 @@ class SyntheseCompetences {
 	private $resiste_maille;
 	private $esquive_plaque;
 	private $resiste_plaque;
+	
+	
 
    function __construct() {
    	$this->parent_id = 0;
---
+++ scripts_python/competences_to_mysql2.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import csv
+import os
+import unicodedata
+import mysql.connector 
+import re
+
+
+f = open('Maitrises-2016.csv')
+if f == None:
+	print("Erreur a l'ouverture du fichier")
+	exit(1)	
+
+
+headers = None
+i = 0
+spamreader = csv.reader(f, delimiter='|', quotechar='"')
+
+try:
+	conn = mysql.connector.connect(host="localhost",user="root",password="my-secret-pw", database="Norlande")
+except Exception as error:
+	print(error)
+	exit(1)
+	
+cursor = conn.cursor()
+            
+
+query="""UPDATE competences 
+SET parcelles=%s, 
+possessions_depart=%s, 
+a_prevoir=%s,
+connaissances=%s,
+aide_jeu=%s,
+maniement=%s,
+attaque_spe=%s,
+attaque_spe_tranchant=%s,
+attaque_spe_contondant=%s,
+attaque_spe_hast=%s,
+attaque_spe_tir=%s,
+attaque_spe_lancer=%s,
+sortilege=%s,
+sort_masse1=%s,
+sort_masse2=%s,
+immunite_etoffe=%s,
+immunite_cuir=%s,
+immunite_maille=%s,
+immunite_plaque=%s,
+amelioration=%s,
+capacite=%s,
+technique1=%s,
+technique2=%s,
+piege1=%s,
+piege2=%s,
+breuvage1=%s,
+breuvage2=%s,
+breuvage3=%s,
+breuvage4=%s,
+breuvage5=%s,
+invocation1=%s,
+invocation2=%s,
+metamorphose=%s,
+pouvoir1=%s,
+pouvoir2=%s,
+pouvoir3=%s,
+pouvoir4=%s
+WHERE competence_nom=%s"""
+           
+
+
+
+for tab_line in spamreader:
+	if i == 0:
+		headers = tab_line
+		i += 1
+		continue
+		
+	maitrise_brut = tab_line[1]
+	
+	nom = tab_line[3]
+	print(nom)
+	
+	parcelle = tab_line[5]
+	possessions_depart = tab_line[6]
+	a_prevoir = tab_line[7]
+	connaissances = tab_line[9]
+	aide_jeu = tab_line[10]
+	maniement = tab_line[19]
+	attaque_spe = tab_line[20]
+	attaque_spe_tranchant = tab_line[21]
+	attaque_spe_contondant = tab_line[22]
+	attaque_spe_hast = tab_line[23]
+	attaque_spe_tir = tab_line[24]
+	attaque_spe_lancer = tab_line[25]
+	sortilege = tab_line[26]
+	sort_masse1 = tab_line[27]
+	sort_masse2 = tab_line[28]
+	immunite_etoffe = tab_line[33]
+	immunite_cuir = tab_line[36]
+	immunite_maille = tab_line[39]
+	immunite_plaque = tab_line[42]
+	
+	amelioration = tab_line[43]
+	capacite = tab_line[44] + " " + tab_line[45]
+	technique1 = tab_line[46] + " ; Coût : " + tab_line[47] + " : " + tab_line[48]
+	technique2 = tab_line[49] + " ; Coût : " + tab_line[50] + " : " + tab_line[51]
+	piege1 = tab_line[52] + " ; Coût : " + tab_line[53] + " : " + tab_line[54]
+	piege2 = tab_line[55] + " ; Coût : " + tab_line[56] + " : " + tab_line[57]
+	breuvage1 = tab_line[58] + " ; Coût : " + tab_line[59] + " : " + tab_line[60]
+	breuvage2 = tab_line[61] + " ; Coût : " + tab_line[62] + " : " + tab_line[63]
+	breuvage3 = tab_line[64] + " ; Coût : " + tab_line[65] + " : " + tab_line[66]
+	breuvage4 = tab_line[67] + " ; Coût : " + tab_line[68] + " : " + tab_line[69]
+	breuvage5 = tab_line[70] + " ; Coût : " + tab_line[71] + " : " + tab_line[72]
+	invocation1 = ttab_line[73] + " ; Coût : " + tab_line[74] + " : " + tab_line[75]
+	invocation2 = tab_line[76] + " ; Coût : " + tab_line[77] + " : " + tab_line[78]
+	metamorphose = tab_line[79]
+	pouvoir1 = tab_line[80] + " : " + tab_line[81]
+	pouvoir2 = tab_line[82] + " : " + tab_line[83]
+	pouvoir3 = tab_line[84] + " : " + tab_line[85]
+	pouvoir4 = tab_line[86] + " : " + tab_line[87]
+	
+	
+	
+	data = (parcelle, possessions_depart, a_prevoir, connaissances, aide_jeu, \
+	maniement, attaque_spe, attaque_spe_tranchant, attaque_spe_contondant, \
+	attaque_spe_hast, attaque_spe_tir, attaque_spe_lancer, sortilege, \
+	sort_masse1, sort_masse2, immunite_etoffe, immunite_cuir, \
+	immunite_maille, immunite_plaque, amelioration, capacite, \
+	technique1, technique2, piege1, piege2, breuvage1, breuvage2, \
+	breuvage3, breuvage4, breuvage5, invocation1, invocation2, \
+	metamorphose, pouvoir1, pouvoir2, pouvoir3, pouvoir4, nom)
+	
+	try:
+		cursor = conn.cursor()
+		cursor.execute(query, data)
+		conn.commit()
+	except Exception as error:
+		print(cursor.statement)
+		#print(data)
+		
+		print(error)
+		exit(1)
+	finally:
+		cursor.close()
+		
+conn.close()
+	
+	
+
+	
+	
+	
+	
