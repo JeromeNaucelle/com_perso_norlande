d7db13aada07e42d15f8485791d6a8fa1dd947e8
Jérôme Naucelle <jerome.naucelle@gmail.com>
2017-01-29 21:10:25 +0100

    rdirection vers la page de login si utilisateur non connecté

--- com_perso_norlande/site/perso_norlande.php
+++ com_perso_norlande/site/perso_norlande.php
@@ -3,11 +3,24 @@
 defined('_JEXEC') or die('Restricted access'); 
 
 // Get an instance of the controller prefixed by HelloWorld
-$controller = JControllerLegacy::getInstance('Perso_Norlande'); 
+$user = JFactory::getUser();
+$status = $user->guest;
 
-// Perform the Request task
-$input = JFactory::getApplication()->input;
-$controller->execute($input->getCmd('task')); 
+if($status == 1){
+	$allDone =& JFactory::getApplication();
+	$allDone->redirect('index.php');
+}
+else
+{
+	//do user logged in stuff
+	$controller = JControllerLegacy::getInstance('Perso_Norlande'); 
+	
+	// Perform the Request task
+	$input = JFactory::getApplication()->input;
+	$controller->execute($input->getCmd('task')); 
+	
+	// Redirect if set by the controller
+	$controller->redirect();
+} 
 
-// Redirect if set by the controller
-$controller->redirect();
+
